67c7b158a435375686f1c0ef4cf03675a02bccdf
Rebecca Wollard <rbwollard@gmail.com>
2023-02-23 11:18:57 -0700

    commit tree 83cbd914cf5df4040cbeb87382f2b0d0bd71a152

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion { in: ['Fiction', 'Non-Fiction'] }
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        if title === "True Facts"
+            errors.add(:title, "title is clickbait")
+        end
+    end
 end
